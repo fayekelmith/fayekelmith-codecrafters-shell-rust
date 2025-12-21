8849e821d440066b5a91b58b0f89b4bafc304b26
kelmith <faye.12222883@lpu.in>
2025-12-22 00:54:23 +0530

    add the exit command

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,21 @@ use std::io::{self, Write};
 fn main() {
 
     loop {
-    let mut input : String = String::new();
-    print!("$ ");
-    io::stdout().flush().unwrap();
+        
+        let mut input : String = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    io::stdin().read_line(&mut input).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim());
+        match input.trim(){
+            "exit" => {
+                break;
+            }
+            _ => {
+                 println!("{}: command not found", input.trim());
+            }
+        }
 
     }
 
