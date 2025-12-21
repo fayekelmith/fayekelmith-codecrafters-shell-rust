f6e3b27e4aa29a2cdb6198ee410760448e0758da
kelmith <faye.12222883@lpu.in>
2025-12-22 01:29:09 +0530

    split at space and work out the echo

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,34 @@
 use std::io::{self, Write};
 
 fn main() {
-
     loop {
         
-        let mut input : String = String::new();
+        let mut input: String = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
 
         io::stdin().read_line(&mut input).unwrap();
 
-        match input.trim(){
-            "exit" => {
-                break;
+        if let Some((first_word, remainder)) = input.trim().split_once(char::is_whitespace){
+            match first_word {
+                "exit" => {
+                    break;
+                }
+                "echo" => {
+                    println!("{}", remainder);
+                }
+                _ => {
+                     println!("{}: command not found", input.trim());
+                }
             }
-            _ => {
-                 println!("{}: command not found", input.trim());
+        }else{
+            match input.trim() {
+                "exit" => {
+                    break;
+                }
+                _ => {
+                     println!("{}: command not found", input.trim());
+                }
             }
         }
 
